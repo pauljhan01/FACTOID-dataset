f7bf94088f9dbe048803ebf2d0f5fb928b0cfcb6
Paul Han <pauljhan@utexas.edu>
2025-10-07 22:02:07 -0500

    use apple silicon gpu

--- src/constants.py
+++ src/constants.py
@@ -3,5 +3,7 @@ import torch
 # define device
 CUDA = 'cuda'
 CPU = 'cpu'
-DEVICE = torch.device(CUDA if torch.cuda.is_available() else CPU)
+DEVICE = torch.device("mps" if torch.mps.is_available() else CPU)
 SEED = 1234
+
+
